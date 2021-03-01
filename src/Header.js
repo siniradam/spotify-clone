8d104db3ea330b173669aa4b831b30e96067e6d0
Koray K <3178435+siniradam@users.noreply.github.com>
2021-03-01 13:33:15 -0800

    Header

---
+++ src/Header.js
@@ -0,0 +1,24 @@
+import { Avatar } from "@material-ui/core";
+import { Search } from "@material-ui/icons";
+import React from "react";
+import { useDataLayerValue } from "./DataLayer";
+import "./Header.css";
+
+function Header() {
+  const [{ user }, dispatch] = useDataLayerValue();
+
+  return (
+    <div className='header'>
+      <div className='header__left'>
+        <Search />
+        <input type='' placeholder='Search for artists, songs ..' />
+      </div>
+      <div className='header__right'>
+        <Avatar src={user?.images[0]?.url} alt='Profile' />
+        <h4>{user?.display_name}</h4>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
